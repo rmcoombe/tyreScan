08e0778f574a9593023c8778504c956e4d4ace26
rmcoombe <rosscoombe@gmail.com>
2020-07-09 15:09:29 +0100

    dscsdc

--- www/js/index.js
+++ www/js/index.js
@@ -41,7 +41,6 @@ cordova.plugins.barcodeScanner.scan(
           prompt : "Place a barcode inside the scan area", // Android
           resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
           formats : "CODE_128", // default: all but PDF_417 and RSS_EXPANDED
-          orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
           disableAnimations : true, // iOS
           disableSuccessBeep: false // iOS and Android
       }
