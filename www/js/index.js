03978826bfe7546f6b01913b8977dffdc01a455b
rmcoombe <rosscoombe@gmail.com>
2020-09-10 14:03:43 +0100

    ioji
    ijoij

--- www/js/index.js
+++ www/js/index.js
@@ -49,6 +49,33 @@ cordova.plugins.barcodeScanner.scan(
    );
 };
 
+function wheelScanU(){
+cordova.plugins.barcodeScanner.scan(
+      function (result) {
+            document.getElementById("wheelNumber").value = result.text;
+            
+          
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : false, // iOS and Android
+          showTorchButton : false, // iOS and Android
+          torchOn: false, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "CODE_128", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
+};
+
+
 function tpmsScan(){
 cordova.plugins.barcodeScanner.scan(
       function (result) {
